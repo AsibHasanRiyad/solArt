93069f645fb8fef817c5d88e1d22c5b19c48eb07
Sayad Riyad <sayadriyad@Sayads-MacBook-Air.local>
2023-12-31 11:40:39 +0600

    project section mobile responsiveness added

--- src/components/OurProject/OurProject.jsx
+++ src/components/OurProject/OurProject.jsx
@@ -14,44 +14,44 @@ const OurProject = () => {
                 {/* image 1 */}
                 <div className=" relative">
                     <img className=" w-[284px] md:w-[384px] lg:w-[589px] " src="https://i.ibb.co/GFdzJsQ/Untitled-design-2.jpg" alt="" />
-                    <div className=" absolute top-10 -right-20 bg-[#252531] w-fit p-5">
-                        <h1 className=" text-[28px] font-semibold text-primary">Architecture</h1>
-                        <p className=" text-[22px] font-medium text-white">Bungalow Dark House</p>
+                    <div className=" absolute top-10 right-2 md:-right-20 bg-[#252531] w-fit p-5">
+                        <h1 className=" text-[22px] md:text-[28px] font-semibold text-primary">Architecture</h1>
+                        <p className=" text-[14px] md:text-[22px] font-medium text-white">Bungalow Dark House</p>
                     </div>
-                    <div className=" text-[22px] font-medium right-28 xl:right-[200px] text-white absolute bottom-5">
+                    <div className=" text-base font-normal md:text-[22px] md:font-medium right-1/2 md:right-28 xl:right-[200px] text-white absolute bottom-5">
                         <h1 className=" text-center" >Bungalow Dark House</h1>
                     </div>
                 </div>
                 {/* image 2 */}
                 <div className=" relative">
-                    <img className=" w-[284px] md:w-[384px] lg:w-[589px] " src="https://i.ibb.co/ctM1YX9/Untitled-design-2-3.jpg" alt="" />
-                    <div className=" absolute bottom-28 -left-20 bg-[#252531] w-fit p-5">
-                        <h1 className=" text-[28px] font-semibold text-primary">Interior Design</h1>
-                        <p className=" text-[22px] font-medium text-white max-w-44">Woodenist House Lumberjack</p>
+                    <img className=" ml-28 md:ml-0 w-[284px] md:w-[384px] lg:w-[589px] " src="https://i.ibb.co/ctM1YX9/Untitled-design-2-3.jpg" alt="" />
+                    <div className=" absolute bottom-28 left-2 md:-left-20 bg-[#252531] w-fit p-5">
+                        <h1 className=" text-[22px]  md:text-[28px] font-semibold text-primary">Interior Design</h1>
+                        <p className=" text-[14px] md:text-[22px] font-medium text-white max-w-44">Woodenist House Lumberjack</p>
                     </div>
-                    <div className=" text-[22px] font-medium right-28 xl:right-[200px] text-white absolute bottom-5">
+                    <div className=" text-base font-normal md:text-[22px] md:font-medium right-10  md:right-28 xl:right-[200px] text-white absolute bottom-5">
                         <h1 className=" text-center" >Woodenist House Lumberjack</h1>
                     </div>
                 </div>
                 {/* image 3 */}
                 <div className=" relative">
                     <img className=" w-[284px] md:w-[384px] lg:w-[589px] " src="https://i.ibb.co/HPTPJk6/2.jpg" alt="" />
-                    <div className=" absolute top-10 -right-20 bg-[#252531] w-fit p-5">
-                        <h1 className=" text-[28px] font-semibold text-primary">Home Decor</h1>
-                        <p className=" text-[22px] font-medium text-white">Bungalow Dark House</p>
+                    <div className=" absolute top-10 right-2 md:-right-20 bg-[#252531] w-fit p-5">
+                        <h1 className=" text-[22px] md:text-[28px] font-semibold text-primary">Home Decor</h1>
+                        <p className=" text-[14px] md:text-[22px] font-medium text-white">Bungalow Dark House</p>
                     </div>
-                    <div className=" text-[22px] font-medium right-28 xl:right-[200px] text-white absolute bottom-5">
+                    <div className=" text-base font-normal md:text-[22px] md:font-medium right-1/2 md:right-28 xl:right-[200px] text-white absolute bottom-5">
                         <h1 className=" text-center" >Bungalow Dark House</h1>
                     </div>
                 </div>
                 {/* image 4 */}
                 <div className=" relative">
-                    <img className=" w-[284px] md:w-[384px] lg:w-[589px] " src="https://i.ibb.co/bHtNkL0/1.jpg" alt="" />
-                    <div className=" absolute bottom-28 -left-20 bg-[#252531] w-fit p-5">
-                        <h1 className=" text-[28px] font-semibold text-primary">Urban Design</h1>
-                        <p className=" text-[22px] font-medium text-white max-w-44">Woodenist House Lumberjack</p>
+                    <img className=" ml-28 md:ml-0 w-[284px] md:w-[384px] lg:w-[589px] " src="https://i.ibb.co/bHtNkL0/1.jpg" alt="" />
+                    <div className=" absolute bottom-28 left-2 md:-left-20 bg-[#252531] w-fit p-5">
+                        <h1 className=" text-[22px]  md:text-[28px] font-semibold text-primary">Urban Design</h1>
+                        <p className=" text-[14px] md:text-[22px] font-medium text-white max-w-44">Woodenist House Lumberjack</p>
                     </div>
-                    <div className=" text-[22px] font-medium right-28 xl:right-[200px] text-white absolute bottom-8">
+                    <div className=" text-base font-normal md:text-[22px] md:font-medium right-10  md:right-28 xl:right-[200px] text-white absolute bottom-5">
                         <h1 className=" text-center" >Woodenist House Lumberjack</h1>
                     </div>
                 </div>
