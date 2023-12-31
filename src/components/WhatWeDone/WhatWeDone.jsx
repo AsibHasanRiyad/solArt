9100772724a5b878c739a78ff104b5514634e7a9
Sayad Riyad <sayadriyad@Sayads-MacBook-Air.local>
2023-12-31 20:34:57 +0600

    what we done section responsiveness added

--- src/components/WhatWeDone/WhatWeDone.jsx
+++ src/components/WhatWeDone/WhatWeDone.jsx
@@ -3,7 +3,8 @@ import Title from "../../shared/Title";
 const WhatWeDone = () => {
   return (
     <div >
-      <div className="max-w-[1440px] mx-auto relative pt-[50px] lg:pt-[120px]">
+      <div className=" relative pt-[50px] lg:pt-[120px]">
+      <div className=" max-w-[1440px] mx-auto">
       <div className="px-4 md:px-14 xl:px-[70px] ">
         <Title
           title={"What We Done"}
@@ -11,6 +12,7 @@ const WhatWeDone = () => {
           heading1={"Let's Have a Look What "}
           heading2={"We Have Done An Amazing Work!"}
         />
+      </div>
       </div>
       {/* image section */}
       <div
@@ -39,16 +41,16 @@ const WhatWeDone = () => {
                 <path
                   d="M21 10C21 17 12 23 12 23C12 23 3 17 3 10C3 7.61305 3.94821 5.32387 5.63604 3.63604C7.32387 1.94821 9.61305 1 12 1C14.3869 1 16.6761 1.94821 18.364 3.63604C20.0518 5.32387 21 7.61305 21 10Z"
                   stroke="white"
-                  stroke-width="2"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
+                  strokeWidth="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
                 />
                 <path
                   d="M12 13C13.6569 13 15 11.6569 15 10C15 8.34315 13.6569 7 12 7C10.3431 7 9 8.34315 9 10C9 11.6569 10.3431 13 12 13Z"
                   stroke="white"
-                  stroke-width="2"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
+                  strokeWidth="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
                 />
               </svg>
               <span className=" text-[28px] font-medium">
@@ -71,8 +73,8 @@ const WhatWeDone = () => {
             opacity="0.3"
             d="M16.4503 18.7259C48.2892 43.0704 130.053 93.9866 202.397 102.896C292.826 114.032 389.895 64.2416 450.031 90.6547C510.166 117.068 510.35 199.785 587.921 203.895C665.492 208.004 695.161 130.88 764.104 129.13"
             stroke="#B98E75"
-            stroke-width="4"
-            stroke-dasharray="8 8"
+            strokeWidth="4"
+            strokeDasharray="8 8"
           />
           <path
             d="M763.855 145.534L763.687 110.764C763.68 109.345 765.112 108.371 766.429 108.898L807.739 125.415C809.382 126.072 809.425 128.382 807.808 129.1L766.666 147.352C765.347 147.937 763.862 146.976 763.855 145.534Z"
@@ -95,7 +97,7 @@ const WhatWeDone = () => {
       {/* mobile svg */}
       <div className=" block md:hidden absolute top-14 right-8">
       <svg xmlns="http://www.w3.org/2000/svg" width="174" height="54" viewBox="0 0 174 54" fill="none">
-  <path opacity="0.3" d="M5.84348 8.63845C12.3249 13.5942 28.9695 23.9592 43.6965 25.7729C62.1052 28.0399 81.8655 17.9041 94.1073 23.281C106.349 28.6579 106.386 45.4966 122.178 46.3332C137.969 47.1698 144.008 31.4697 158.043 31.1133" stroke="#B98E75" stroke-width="1.5" stroke-dasharray="8 8"/>
+  <path opacity="0.3" d="M5.84348 8.63845C12.3249 13.5942 28.9695 23.9592 43.6965 25.7729C62.1052 28.0399 81.8655 17.9041 94.1073 23.281C106.349 28.6579 106.386 45.4966 122.178 46.3332C137.969 47.1698 144.008 31.4697 158.043 31.1133" stroke="#B98E75" strokeWidth="1.5" strokeDasharray="8 8"/>
   <path d="M158.08 33.3763L158.049 26.969C158.042 25.5501 159.474 24.5755 160.791 25.1023L168.404 28.146C170.047 28.8031 170.09 31.1136 168.473 31.8313L160.891 35.1948C159.572 35.7798 158.087 34.8189 158.08 33.3763Z" fill="#B98E75"/>
   <g opacity="0.4">
     <circle cx="127.488" cy="13.9179" r="4.68841" stroke="#B98E75"/>
