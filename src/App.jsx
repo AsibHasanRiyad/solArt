53967e6be3d17181d9973caf4685d199066d9a9d
Sayad Riyad <sayadriyad@Sayads-MacBook-Air.local>
2023-12-31 08:31:00 +0600

    contact section is added

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 
 import About from "./components/About/About";
 import Banner from "./components/Banner/Banner";
+import Contact from "./components/Contact/Contact";
 import Footer from "./components/Footer/Footer";
 import MeetOurTeam from "./components/MeetOurTeam/MeetOurTeam";
 import Navbar from "./components/Navbar/Navbar";
@@ -16,6 +17,7 @@ const App = () => {
         <About />
         <MeetOurTeam />
         <WhatWeDone />
+        <Contact />
         <Footer />
       </div>
   );
---
+++ src/components/Contact/Contact.jsx
@@ -0,0 +1,66 @@
+const Contact = () => {
+  return (
+    <div>
+      <div className=" hidden md:block h-[2px] bg-primary"></div>
+      {/* contact info div */}
+      <div className=" flex items-center">
+        <div className=" flex-auto flex justify-center items-center pt-10 pb-16 md:border-r-2 mt-6 md:mt-2 md:border-r-primary ">
+          <div>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 108 10"
+              fill="none"
+              className="w-[64px] h-[6.5px] md:w-[108px] md:h-[10px] "
+            >
+              <rect y="0.5" width="88" height="3" fill="#B98E75" />
+              <rect x="20" y="6.5" width="88" height="3" fill="#B98E75" />
+            </svg>
+            <div className=" pt-6">
+              <h1 className=" text-primary">Phone</h1>
+              <h1 className=" text-heading"> +1 203-123-0606</h1>
+            </div>
+          </div>
+        </div>
+        <div className=" h-24 w-[1px] bg-primary"></div>
+        {/*  */}
+        <div className=" flex-auto flex justify-center items-center pt-10 pb-16 md:border-r-2 mt-6 md:mt-2 border-r-primary ">
+          <div>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 108 10"
+              fill="none"
+              className="w-[64px] h-[6.5px] md:w-[108px] md:h-[10px] "
+            >
+              <rect y="0.5" width="88" height="3" fill="#B98E75" />
+              <rect x="20" y="6.5" width="88" height="3" fill="#B98E75" />
+            </svg>
+            <div className=" pt-6">
+              <h1 className=" text-primary">Email </h1>
+              <h1 className=" text-heading"> architecture@bauen.com</h1>
+            </div>
+          </div>
+        </div>
+        {/*  */}
+        <div className=" hidden flex-auto md:flex justify-center items-center pt-10 pb-16 mt-2 ">
+          <div>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 108 10"
+              fill="none"
+              className="w-[64px] h-[6.5px] md:w-[108px] md:h-[10px] "
+            >
+              <rect y="0.5" width="88" height="3" fill="#B98E75" />
+              <rect x="20" y="6.5" width="88" height="3" fill="#B98E75" />
+            </svg>
+            <div className=" pt-6">
+              <h1 className=" text-primary">Our Address </h1>
+              <h1 className=" text-heading">24 King St, Charleston, SC 29401 USA</h1>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Contact;
